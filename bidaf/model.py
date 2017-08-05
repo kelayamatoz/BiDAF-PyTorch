0adc93002946cd7633430dce52face6400cb9301
Tian Zhao <tianzhao@dawn13.stanford.edu>
2017-08-05 00:30:35 -0700

    working on model for bidaf

--- bidaf/model.py
+++ bidaf/model.py
@@ -9,13 +9,20 @@ from torch.autograd import Variable
 
 # TODO: Multi GPU settings?
 
+dtype = torch.FloatTensor
+
 class BiDAF(nn.Module):
     def __init__(self, config):
         super(BiDAF, self).__init__()
         self.config = config
-
+        N, M, JX, JQ, VW, VC, W = \
+            config.batch_size, config.max_num_sents, config.max_sent_size, \
+            config.max_ques_size, config.word_vocab_size, config.char_vocab_size, config.max_word_size
+        
     def forward(self, x, cx, x_mask, q, cq, q_mask, new_emb_mat=None):
         print("in forward function of BiDAF, printing x:")
         print(x)
         print("q")
         print(q)
+        print("emb_mat")
+        print(new_emb_mat)
